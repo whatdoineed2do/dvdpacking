fbbb699695ac9f62f0ddea4a31f72547bacc45bb
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2022-12-17 21:16:01 +0000

    switch readdir to c++17 filesystem if available

--- src/Item.cc
+++ src/Item.cc
@@ -3,13 +3,17 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <dirent.h>
 #include <limits.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 
 #include <sstream>
+#if HAVE_CXX17 == 1
+#include <filesystem>
+#else
+#include <dirent.h>
+#endif
 
 
 Item::Item(const char* what_)
@@ -52,6 +56,26 @@ off_t  Item::_du(const char* where_) const
 {
     LOG_DEBUG("examinging dir " << where_);
     off_t  size = 0;
+
+ #if HAVE_CXX17 == 1
+    if (!std::filesystem::exists(where_)) {
+        std::ostringstream  err;
+        err << "failed to open '" << where_ << "' - " << strerror(errno);
+        throw std::invalid_argument(err.str());
+    }
+
+    for (const auto& de : std::filesystem::directory_iterator{where_,
+                                                              std::filesystem::directory_options::follow_directory_symlink}
+	)
+    {
+	if (de.symlink_status().type() == std::filesystem::file_type::directory) {
+	    size += _du(de.path().c_str());
+	}
+	else {
+	    size += de.file_size();
+	}
+    }
+#else
     DIR*  d;
     if ( (d = opendir(where_)) == nullptr) {
         std::ostringstream  err;
@@ -97,6 +121,7 @@ off_t  Item::_du(const char* where_) const
     }
     free(dent);
     closedir(d);
+#endif
 
     LOG_DEBUG(where_ << " = " << size);
     return size;
